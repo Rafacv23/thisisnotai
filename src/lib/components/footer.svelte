5c8a514eb7a34047ba4fff9352580f010770c08c
Rafa <rafacv23@gmail.com>
2025-11-04 17:29:57 +0100

    footer template

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,6 @@
+<footer
+	class="mt-16 flex flex-col items-center rounded-xl bg-primary px-8 py-4 text-primary-foreground shadow-sm"
+>
+	<h4>thisisnotai.xyz</h4>
+	<p>Made with love by Rafa Canosa</p>
+</footer>
