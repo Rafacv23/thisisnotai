81fcf08a9999958edca6e4bf78c54ab72bbb6dc9
Rafa <rafacv23@gmail.com>
2025-11-04 17:30:31 +0100

    removed svelte template texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,4 @@
 	import Hero from '$lib/components/hero.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 <Hero />
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
