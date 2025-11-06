368412c083cf0650976d00989a086278bba16329
Rafa <rafacv23@gmail.com>
2025-11-06 17:52:39 +0100

    wraped the features in and for each

--- src/lib/components/features.svelte
+++ src/lib/components/features.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
 	import { m } from '$lib/paraglide/messages.js';
+
+	const FEATURES = [
+		{
+			heading: m.feat_1_heading,
+			text: m.feat_1_p,
+			animate: true
+		},
+		{
+			heading: m.feat_2_heading,
+			text: m.feat_2_p
+		},
+		{
+			heading: m.feat_3_heading,
+			text: m.feat_3_p
+		},
+		{
+			heading: m.feat_4_heading,
+			text: m.feat_4_p
+		},
+		{
+			heading: m.feat_5_heading,
+			text: m.feat_5_p
+		}
+	];
 </script>
 
 <section class="mx-auto mb-16 text-base text-foreground/80">
-	<h3>{m.feat_1_heading()}</h3>
-	<p>{m.feat_1_p()}</p>
-	<h3>{m.feat_2_heading()}</h3>
-	<p>{m.feat_2_p()}</p>
-	<h3>{m.feat_3_heading()}</h3>
-	<p>{m.feat_3_p()}</p>
-	<h3>{m.feat_4_heading()}</h3>
-	<p>{m.feat_4_p()}</p>
-	<h3>{m.feat_5_heading()}</h3>
-	<p>{m.feat_5_p()}</p>
+	<ul>
+		{#each FEATURES as feat, i (i)}
+			<li>
+				<h3>{feat.heading()}</h3>
+				<p>{feat.text()}</p>
+			</li>
+		{/each}
+	</ul>
 </section>
