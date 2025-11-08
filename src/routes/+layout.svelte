d1a434016b6bff32688d5f7e222de82cb176c65d
Rafa <rafacv23@gmail.com>
2025-11-08 18:11:00 +0100

    added header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
 
@@ -16,5 +17,6 @@
 <Toaster />
 
 <ModeWatcher />
+<Header />
 {@render children()}
 <Footer />
